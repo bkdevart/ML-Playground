4abd8dc6a87330012bc0d10d9c74bc6f1895e344
Brandon Knox <bknox.dev@gmail.com>
2023-02-17 05:54:17 -0700

    Editor is making a mess out of code I commented out, tried to clear everything and start with another project's interface as a code base

--- ML Playground/ContentView.swift
+++ ML Playground/ContentView.swift
@@ -3,91 +3,58 @@
 //  ML Playground
 //
 //  Created by Brandon Knox on 2/16/23.
-//
-
-import SwiftUI
-import CoreData
-
 struct ContentView: View {
-//    @Environment(\.managedObjectContext) private var viewContext
-//
-//    @FetchRequest(
-//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-//        animation: .default)
-//    private var items: FetchedResults<Item>
 
+    let model = SleepCalculator()
+    
     var body: some View {
-        VStack{
-            Text("When do you want to wake up?")
-                        .font(.headline)
-        }
-        
-//        NavigationView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                    } label: {
-//                        Text(item.timestamp!, formatter: itemFormatter)
+        NavigationView {
+            Form {
+                Text("Sleep Time: \(alertMessage)")
+                    .font(.headline)
+                
+                Section (header: Text("When do you want to wake up?")
+                            .font(.headline)) {
+
+                    DatePicker("Please enter a time",
+                               selection: $wakeUp,
+                               displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .onChange(of: wakeUp) { newValue in calculateBedtime() }
+                }
+                
+                Section(header: Text("Desired amount of sleep")
+                            .font(.headline)) {
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                    .onChange(of: sleepAmount) { newValue in calculateBedtime() }
+                    .accessibility(value: Text("\(String(format: "%.2f", sleepAmount)) hours"))
+                }
+                
+                Section(header: Text("Daily coffee intake")
+                            .font(.headline)) {
+                    
+//                    Picker("Coffee", selection: $coffeeAmount) {
+//                        ForEach(1..<21) { cup in
+//                            Text("\(cup) cups")
+//                        }
 //                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//            Text("Select an item")
+//                    .onChange(of: coffeeAmount) { newValue in calculateBedtime() }
+                    
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
+                    }
+                    .onChange(of: coffeeAmount) { newValue in calculateBedtime() }
+                    .accessibility(value: Text("\(coffeeAmount) cups"))
+                }
+            }
+            .navigationBarTitle("BetterRest")
         }
     }
-
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            offsets.map { items[$0] }.forEach(viewContext.delete)
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//}
-//
-//private let itemFormatter: DateFormatter = {
-//    let formatter = DateFormatter()
-//    formatter.dateStyle = .short
-//    formatter.timeStyle = .medium
-//    return formatter
-//}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
