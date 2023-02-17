d6fd01f72845382a326db7f30b7e85e86b8cada5
Brandon Knox <bknox.dev@gmail.com>
2023-02-17 06:35:45 -0700

    Added ML model to project, disabled code review from editor

--- ML Playground/ContentView.swift
+++ ML Playground/ContentView.swift
@@ -5,13 +5,48 @@
 //  Created by Brandon Knox on 2/16/23.
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
-
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
 //    let model = SleepCalculator()
     
     var body: some View {
         NavigationView {
-        }
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
     }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try DiabetesTest(configuration: config)
+
+            // more code here
+        } catch {
+            // something went wrong!
+        }
+    }
+}
